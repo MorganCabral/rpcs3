13e71291e11a0bdf6bf67f4a89ae1a7a0b9eba8b
MorganCabral <morgan.cabral@gmail.com>
2014-04-15 22:53:31 -0400

    Added X11 as a dependency in CMake. Modified some compiler flags so that the build supports Clang. Reworked some macros so that they will at least compile on Mac OS X. I still need to come back to these later and rework them a bit since technically they aren't doing quite the same thing - the goal right now is to get this thing compiling on Mac OS. Then we can worry about functionality.

--- Utilities/GNU.h
+++ Utilities/GNU.h
@@ -3,8 +3,13 @@
 #if defined(__GNUG__)
 #include <cmath>
 #include <stdlib.h>
-#include <malloc.h>
+#endif
 
+#if !defined(__APPLE__)
+#include <malloc.h>
+#endif
+
+#if defined(__GNUG__)
 #define _fpclass(x) std::fpclassify(x)
 #define __forceinline __attribute__((always_inline))
 #define _byteswap_ushort(x) __builtin_bswap16(x)
@@ -16,7 +21,14 @@
 #define _CRT_ALIGN(x) __attribute__((aligned(x)))
 #define InterlockedCompareExchange(ptr,new_val,old_val)  __sync_val_compare_and_swap(ptr,old_val,new_val)
 #define InterlockedCompareExchange64(ptr,new_val,old_val)  __sync_val_compare_and_swap(ptr,old_val,new_val)
+#if defined(__APPLE__)
+// HACK - Relies on OS X's default alignment of 16 bytes.
+// Not entirely sure if this is appropriate; posix_memalign
+// would be better.
+#define _aligned_malloc(size,alignment) malloc(size) 
+#else
 #define _aligned_malloc(size,alignment) memalign(alignment,size)
+#endif
 #define _aligned_free  free
 #define DWORD int32_t
 #endif
